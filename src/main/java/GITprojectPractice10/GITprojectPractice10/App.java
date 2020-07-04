6109fa6e7fc6b4e45f15f3cf63cd369ddd76dedc
NataLiaKech <nkechedzhieva@gmail.com>
2020-07-03 22:34:49 -0400

    practice git10

---
+++ src/main/java/GITprojectPractice10/GITprojectPractice10/App.java
@@ -0,0 +1,13 @@
+package GITprojectPractice10.GITprojectPractice10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GITprojectPractice10/GITprojectPractice10/AppTest.java
@@ -0,0 +1,20 @@
+package GITprojectPractice10.GITprojectPractice10;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
